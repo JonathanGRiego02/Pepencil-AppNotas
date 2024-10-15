f5abe2c6f145052b4aa4b1fb78f5652f01024302
JonathanGRiego02 <jonathangrclases@gmail.com>
2024-10-15 11:30:46 +0100

    Mejora iconos y rootcontroller

---
+++ src/main/java/dad/pepencil/controllers/RootController.java
@@ -0,0 +1,33 @@
+package dad.pepencil.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RootController  implements Initializable {
+    @FXML
+    private TabPane editionTabPane;
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    void onNewAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOpenAction(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    elos 
+    }
+}
